95b88ae698a27d6b53ebd12704713993e8c591a5
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-12-06 16:43:00 +0100

    implementing methods in node

--- src/main/java/gcom/Node.java
+++ src/main/java/gcom/Node.java
@@ -1,10 +1,40 @@
 package gcom;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 import communication.IMulticaster;
+import group.NameServer;
 import order.IOrderer;
 import order.Message;
 
-public class Node implements GCom, INode {
+public class Node extends UnicastRemoteObject implements GCom, INode {
+	
+
+
+	private static final long serialVersionUID = 6210826964208775888L;
+	private String name;
+	private String nameServerHost;
+	private NameServer nameServer;
+	private Registry remoteRegistry;
+	private IOrderer orderer;
+	private IMulticaster multicaster;
+	
+	
+	
+	public Node(String name, String nameServerHost) throws RemoteException {
+		this.name = name;
+		this.nameServerHost = nameServerHost;
+		try {
+			remoteRegistry = LocateRegistry.getRegistry(nameServerHost);
+			nameServer = (NameServer) remoteRegistry.lookup(NameServer.nameServer);
+		} catch (RemoteException | NotBoundException e) {
+			e.printStackTrace();
+		} 
+	}
 
 	@Override
 	public int getId() {
@@ -38,7 +68,8 @@ public class Node implements GCom, INode {
 
 	@Override
 	public void setConfig(IOrderer orderer, IMulticaster multicaster) {
-		// TODO Auto-generated method stub
+		this.orderer = orderer;
+		this.multicaster = multicaster;
 
 	}
 
