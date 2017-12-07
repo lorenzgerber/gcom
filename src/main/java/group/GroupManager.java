19158aa9915754794dc387c92a4b0117fad67378
Lennart Jern <lennart.jern@gmail.com>
2017-12-07 09:46:13 +0100

    Added method stubs to GroupManager

--- src/main/java/group/GroupManager.java
+++ src/main/java/group/GroupManager.java
@@ -1,5 +1,47 @@
 package group;
 
+import java.util.UUID;
+
+import gcom.INode;
+
 public class GroupManager {
 
+	/**
+	 * Join the specified group.
+	 * 
+	 * @param group
+	 *            name of the group to join
+	 * @return the assigned node ID in this group
+	 */
+	public UUID join(String group) {
+		// TODO implement this
+		return null;
+	}
+
+	/**
+	 * Add the given node to the group. When called on the leader, this should make
+	 * all members add the new node.
+	 * 
+	 * @param node
+	 */
+	public void addToGroup(INode node) {
+		// TODO implement this
+	}
+
+	/**
+	 * Leave the current group.
+	 */
+	public void leave() {
+		// TODO implement this
+	}
+
+	/**
+	 * Send some data to all members in the group.
+	 * 
+	 * @param data
+	 *            the data to send
+	 */
+	public <T> void send(T data) {
+		// TODO implement this
+	}
 }
