a2af0d329bd242c3359ef52bcbb46dcf295c98af
Lennart Jern <lennart.jern@gmail.com>
2017-12-07 18:41:56 +0100

    Initial work on GroupManager::addToGroup

--- src/main/java/group/GroupManager.java
+++ src/main/java/group/GroupManager.java
@@ -1,6 +1,9 @@
 package group;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.UUID;
 
 import gcom.INode;
@@ -10,10 +13,13 @@ public class GroupManager {
 	INameServer nameServer;
 	INode parent;
 	boolean isLeader = true;
+	List<INode> peers;
 
 	public GroupManager(INameServer nameServer, INode parent) {
 		this.nameServer = nameServer;
 		this.parent = parent;
+		peers = new ArrayList<>();
+		peers.add(parent);
 	}
 
 	/**
@@ -37,7 +43,8 @@ public class GroupManager {
 				uuid = leader.addToGroup(parent);
 			}
 		} catch (RemoteException e1) {
-			System.err.println("The name service is down! Unable to continue, exiting...");
+			// TODO: Should we quit or throw exception here?
+			System.err.println("The name service is down! Unable to continue...");
 			System.exit(-1);
 		}
 		return uuid;
@@ -48,9 +55,43 @@ public class GroupManager {
 	 * all members add the new node.
 	 * 
 	 * @param node
+	 * @return the uuid of the new member if this is the leader, null otherwise.
 	 */
-	public void addToGroup(INode node) {
-		// TODO implement this
+	public UUID addToGroup(INode node) {
+		UUID uuid = null;
+		if (isLeader) {
+			Iterator<INode> iter = peers.iterator();
+			while (iter.hasNext()) {
+				INode peer = iter.next();
+				// Do not call addToGroup on self
+				if (peer.equals(parent)) {
+					continue;
+				}
+
+				// Add the new node to each group member and add all members to the new node.
+				try {
+					peer.addToGroup(node);
+				} catch (RemoteException e) {
+					removeMember(peer);
+				}
+				try {
+					node.addToGroup(peer);
+				} catch (RemoteException e) {
+					removeMember(node);
+				}
+			}
+
+			try {
+				node.addToGroup(parent);
+			} catch (RemoteException e) {
+				removeMember(node);
+			}
+			peers.add(node);
+		} else {
+			peers.add(node);
+		}
+
+		return uuid;
 	}
 
 	/**
@@ -67,6 +108,10 @@ public class GroupManager {
 	 *            the data to send
 	 */
 	public <T> void send(T data) {
+		// TODO implement this
+	}
+
+	private void removeMember(INode member) {
 		// TODO implement this
 	}
 }
