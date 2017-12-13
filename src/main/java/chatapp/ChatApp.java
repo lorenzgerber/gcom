3e069f08116a12717cc6eb8e19528e286ea2d4e7
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-12-13 16:52:58 +0100

    javafx skeleton

--- src/main/java/chatapp/ChatApp.java
+++ src/main/java/chatapp/ChatApp.java
@@ -1,5 +1,28 @@
 package chatapp;
 
-public class ChatApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ChatApp extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		  Parent root = 
+			         FXMLLoader.load(getClass().getResource("chatapp.fxml"));
+
+			      Scene scene = new Scene(root); 
+			      stage.setTitle("Chat App"); 
+			      stage.setScene(scene);
+			      stage.show(); 
+			   }
+
+			   public static void main(String[] args) {
+			      
+			      launch(args); 
+			   }
+	
 
 }
