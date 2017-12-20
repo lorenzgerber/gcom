14fcf6a2053b3b482e7cc6b438b44cb27f84755d
Lennart Jern <lennart.jern@gmail.com>
2017-12-20 09:13:27 +0100

    Basic connection between gui and GCom, possible to send message to self

--- src/main/java/chatapp/ChatAppController.java
+++ src/main/java/chatapp/ChatAppController.java
@@ -1,31 +1,49 @@
 package chatapp;
 
+import java.rmi.RemoteException;
+
+import gcom.GCom;
+import gcom.ISubscriber;
+import gcom.Node;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class ChatAppController {
-	
-	@FXML 
-	private TextField inputArea; 
+public class ChatAppController implements ISubscriber {
 
 	@FXML
-	private TextArea messageArea; 
-	
+	private TextField inputArea;
+
+	@FXML
+	private TextArea messageArea;
+
+	private GCom node;
+
+	public ChatAppController() {
+		try {
+			node = new Node("localhost");
+			node.subscribe(this);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	@FXML
 	private void sendButtonPressed(ActionEvent event) {
-		// do some stuff on button press
-		System.out.println("send the text to everybody");
+		node.Send(inputArea.getText());
+		inputArea.clear();
 	}
-	
-	
-	
-	
 
-	
 	public void initialize() {
-		
+
+	}
+
+	@Override
+	public <T> void deliverMessage(T message) {
+		String msg = (String) message;
+		messageArea.appendText(msg + "\n");
 	}
 
 }
