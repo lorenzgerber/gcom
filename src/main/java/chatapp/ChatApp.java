29d751ae12a1160f2ad8d9f349081a5ec6b6c338
Lorenz Gerber <genfys@gmail.com>
2017-12-22 20:53:36 +0100

    add app closing code

--- src/main/java/chatapp/ChatApp.java
+++ src/main/java/chatapp/ChatApp.java
@@ -2,15 +2,27 @@ package chatapp;
 
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ChatApp extends Application {
+	
+	Stage primaryStage;
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		
+		primaryStage = stage;
+		primaryStage.setOnCloseRequest(e -> {
+			Platform.exit();
+			System.exit(0);
+		});
+		
+		
+		
 		  Parent root = FXMLLoader.load(getClass().getResource("StartMenu.fxml"));
 		  
 		  Scene scene = new Scene(root);
