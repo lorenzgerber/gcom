736637a20cab0f195c115f534145b8b91bf76197
Lorenz Gerber <lorenzottogerber@gmail.com>
2018-01-03 22:57:08 +0100

    preparing for implementation of debug group manager

---
+++ src/main/java/group/IGroupManager.java
@@ -0,0 +1,32 @@
+package group;
+
+import gcom.INode;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface IGroupManager {
+	
+	public void join(String group) throws RemoteException;
+	
+	public void addToGroup(INode node);
+	
+	public void removeMember(INode node);
+	
+	public void leave();
+	
+	public <T> void send(T data);
+	
+	public void tryRemoveFromGroup(INode member);
+	
+	public void tryToAdd(INode added, INode receiver);
+	
+	public void updateLeader();
+	
+	public void electLeader(INode newLeader) throws RemoteException;
+	
+	public boolean isLeader();
+	
+	public List<String> getGroups() throws RemoteException;
+
+}
